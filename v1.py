7e1ab735d0406109e674b7914c5c61b5793c8e22
rojassanchezalondra384-max <rojassanchezalondra384@gmail.com>
2025-12-10 12:08:31 -0600

    Esta el mi primera versión de codigo
    este código se encarga de mostrar una ventana de diferentes características, las cuales son:
    ° Título "Detección de adicción a TikTok"
    ° Medidas: "250x300"
    ° Color: este fue a elección y nosotros elegimos el color blanco "#9EB3A4"

---
+++ v1.py
@@ -0,0 +1,11 @@
+#importar las  bibliotecas 
+import tkinter as tk 
+#configurar la ventana
+ventana = tk.Tk()
+ventana.title("E4 3CP Test de adicciones a TikTok") # agreganos titulo a la ventana
+ventana.geometry("250x300") # agregamos medidas a la ventana 
+ventana.config(bg="#9EB3A4") # agreganos color a la ventana
+
+
+#Mandar a traer mi ventana
+ventana.mainloop()
